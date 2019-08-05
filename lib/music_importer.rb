ab5ec265c324cf669798a4cc675e4aca8c350b8d
Veronica Lopez <nicaa0695@gmail.com>
2019-08-05 18:16:37 +0000

    Automatically backed up by Learn

---
+++ lib/music_importer.rb
@@ -0,0 +1,21 @@
+  class MusicImporter
+   attr_reader :path
+
+   # initialize -accepts a file path to a directory of MP3 files.
+
+    def initialize(path)
+     @path = path
+   end
+
+  # files - returns all of the imported filenames.
+
+    def files
+     @files ||= Dir.glob("#{path}/*.mp3").collect{|f| f.gsub("#{path}/", "")}
+   end
+
+  # import - imports all of the files from the library, instantiating a new Song object for each file.
+
+    def import
+     files.each{|f| Song.create_from_filename(f)}
+   end
+ end 
